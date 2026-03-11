85e15c44e15b6b22116b7bdfe6498853cbc5858a
Damiensiri <damiensiri.pro@hotmail.fr>
2026-03-11 17:59:44 +0100

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,21 +1,43 @@
-importScripts("https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js");
-importScripts("https://www.gstatic.com/firebasejs/9.23.0/firebase-messaging-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/9.23.0/firebase-app-compat.js")
+importScripts("https://www.gstatic.com/firebasejs/9.23.0/firebase-firestore-compat.js")
 
 firebase.initializeApp({
 apiKey:"AIzaSyBn6Hgu-xvsWqpwJ-i4lZyI0QjKMBvOeXA",
 authDomain:"ecurie-paddock.firebaseapp.com",
 projectId:"ecurie-paddock",
+storageBucket:"ecurie-paddock.firebasestorage.app",
 messagingSenderId:"501153278739",
 appId:"1:501153278739:web:824b4344c381f2a94548e6"
-});
+})
 
-const messaging=firebase.messaging();
+const db = firebase.firestore()
 
-messaging.onBackgroundMessage(function(payload){
+let initialized=false
 
-self.registration.showNotification(payload.notification.title,{
-body:payload.notification.body,
+if(!initialized){
+
+initialized=true
+
+db.collection("reservations").onSnapshot(snapshot=>{
+
+snapshot.docChanges().forEach(change=>{
+
+if(change.type==="added"){
+
+const r=change.doc.data()
+
+self.registration.showNotification(
+"Nouvelle réservation paddock",
+{
+body:r.name+" — "+r.paddock+" "+r.time,
 icon:"/icon.png"
-});
+}
+)
 
-});
+}
+
+})
+
+})
+
+}
